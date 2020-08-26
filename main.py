903263736c96947c78ad69c707277d0e4526251b
Robert Stark <61947842+starkex@users.noreply.github.com>
2020-08-26 17:04:38 +0530

    generator

--- main.py
+++ main.py
@@ -5,17 +5,26 @@ win.title('Jarvis')
 
 userl = ttk.Label(win,text='Username : ')
 userl.grid(row=0,column=0)
-
-userf =ttk.Entry(win,width=20)
+uservar=tk.StringVar()
+userf =ttk.Entry(win,width=20,textvariable=uservar)
 userf.grid(row=0,column=1)
 
 passl = ttk.Label(win,text='Password  : ')
 passl.grid(row=1,column=0)
 
-passf =ttk.Entry(win,width=20)
+passvar = tk.StringVar()
+passf = ttk.Entry(win, width=20, textvariable=passvar)
 passf.grid(row=1,column=1)
 
+choice=ttk.Label(win,text='Suggestion')
+choice.grid(row=4,column=0)
+choicebutton=ttk.Button(win,text='Generate Password')
+choicebutton.grid(row=4,column=1)
+
+per=ttk.Radiobutton(win,text='Public')
+per.grid(row=5,column=1)
+
 sub = ttk.Button(win,text='Submit')
-sub.grid(row=4,column=1)
+sub.grid(row=6,column=1)
 
 win.mainloop()
